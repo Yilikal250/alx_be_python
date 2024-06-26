2a344f0d714e376ac2654a2a62e5618409991070
Yilikal250 <teshomeyilikal250@gmail.com>
2024-06-26 09:32:00 +0200

    indentation and spelling corrected

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if weather == "sunny":
-    WearThis = " Wear a t-shirt and sunglasses. "
+if weather == '"sunny"':
+    WearThis = " Wear a t-shirt and sunglasses. " 
 elif weather == "cold":
     WearThis = " Don’t forget your umbrella and a raincoat. "
 else:
-    WearThis = " Make sure to wear a warm coat and a scarf."
+    WearThis = " Make sure to wear a warm coat and a scarf. "
 
 print(WearThis)
