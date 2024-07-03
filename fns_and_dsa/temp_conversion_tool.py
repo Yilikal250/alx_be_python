aca0abd41632899f02c02730d1ee005092bfaa4a
Yilikal250 <teshomeyilikal250@gmail.com>
2024-07-03 17:18:21 +0200

    fahranite to celsius and viceverse conversion add

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,15 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celsius
+def convert_to_fahrenheit(celsius):
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit
+
+temp = float(input("Enter the temperature to convert: "))
+tempchoose=input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+if tempchoose == "F":
+   print(convert_to_celsius(temp),"°F")
+else:
+   print(convert_to_fahrenheit(temp),"°C")    
