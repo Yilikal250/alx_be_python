b869624ae1d6594aea6c4e9eed8ecc9c4187a1e7
Yilikal250 <teshomeyilikal250@gmail.com>
2024-07-09 19:58:46 +0200

    multiplication function name corrected

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,7 +16,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Test the addition method."""
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(1, 1), 0)    
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the addition method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
