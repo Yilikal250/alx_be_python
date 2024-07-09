0050718f4e7856ef6b0dbaf6f35349c789990b19
Yilikal250 <teshomeyilikal250@gmail.com>
2024-07-09 18:18:29 +0200

    try catch added

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,8 @@
 def safe_divide(numerator, denominator):
   numerator = int(numerator)
   denominator = int (denominator)  
-  if denominator == 0:
-    raise ZeroDivisionError("Division by zero is not allowed")
-  return numerator / denominator
+  try:
+    return numerator / denominator 
+  except ZeroDivisionError as e:
+    return e
+  
