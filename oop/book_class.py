673f22776c39c2502fe599b9d1a2788bcc07deea
Yilikal250 <teshomeyilikal250@gmail.com>
2024-07-18 22:33:10 +0200

    book added and tasted magic method

---
+++ oop/book_class.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self,title: str, author: str,year: int ):
+               self.title = title
+               self.author =  author
+               self.year = year
+    def __str__(self):
+        return f"{self.year} by {self.title}, published in {self.year}"           
+    def __repr__(self):
+        return f"Book(title ='{self.title}', author = '{self.author}', year = {self.year})"    
+    def __del__(self):
+       return f"Deleting {self.title}" 
+    
